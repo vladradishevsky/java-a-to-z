91f8c8998bc93e82469d80733998289fb4aaba87
vladradishevsky <rvl@myttk.ru>
2016-11-04 19:46:14 +0700

    new commit

--- learning/src/main/java/rali/User.java
+++ learning/src/main/java/rali/User.java
@@ -2,8 +2,15 @@ package rali;
 
 public class User {
 	public String name;
-	
+
+    public User() {
+
+    }
 	public User(String name) {
 		this.name = name;
+	}
+
+	public String getName()	{
+		return  this.name;
 	}
 }
