ecad428a6171dc32206d7ce9a9b6556389b8456a
vladradishevsky <rvl@myttk.ru>
2017-01-02 03:09:26 +0700

    menu is ready

--- chapter_003/lesson_1/src/main/java/radishevsky/ChatBot.java
+++ chapter_003/lesson_1/src/main/java/radishevsky/ChatBot.java
@@ -1,6 +1,8 @@
 package radishevsky;
 
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -9,11 +11,17 @@ import java.util.Scanner;
 public class ChatBot {
 
     public static void main(String[] args) {
-        new ChatBot().init();
+
+        try {
+            new ChatBot().init();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("Конец программы");
     }
 
-    public void init() {
+    public void init() throws Exception {
 
         String message;
         RandomMessenger messenger = new RandomMessenger();
@@ -39,13 +47,50 @@ public class ChatBot {
     }
 }
 class RandomMessenger {
-    Scanner scanner = new Scanner(System.in);
 
-    public String ask(String question) {
+    Scanner scanner;
+    // FileInputStream answers;
+    FileOutputStream log;
+    Scanner answers;
+
+    RandomMessenger() throws FileNotFoundException {
+        this.scanner = new Scanner(System.in);
+        // answers = new FileInputStream("D:\\answers.txt");
+        this.answers = new Scanner(new File("D:\\answers.txt"));
+        this.log = new FileOutputStream("D:\\log.txt");
+    }
+
+    public String ask(String question) throws IOException {
         System.out.print(question);
-        return scanner.nextLine();
+        String message = this.scanner.nextLine();
+        // log.write(message.getBytes("UTF-8"));
+        this.log.write((String.format("[%s]User: %s\n", new Date(), message)).getBytes("UTF-8"));
+
+        return message;
     }
-    public void answer() {
-        System.out.println("Ответ");
+    public void answer() throws UnsupportedEncodingException, IOException {
+        String message = this.getRandomStringFromAnswers();
+        this.log.write((String.format("[%s]Bot: %s\n", new Date(), message)).getBytes("UTF-8"));
+        System.out.println(message);
     }
+
+    private String getRandomStringFromAnswers() {
+
+        String result = "Check answers file, may be it's empty";
+
+        int randomInt = 1 + Math.round((float)Math.random() * 9);
+
+        System.out.println("randomInt: " + randomInt);
+        // System.out.println("random int is " + randomInt);
+
+        for (int index = 0; index < randomInt; index++) {
+            if (this.answers.hasNext()) {
+                result = this.answers.nextLine();
+            } else {
+                this.answers.reset();
+            }
+        }
+        return result;
+    }
+
 }
