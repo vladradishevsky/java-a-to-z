c61903cb72cb9afc5e020dfe63746326cecf9e7a
vladradishevsky <rvl@myttk.ru>
2017-02-16 19:32:08 +0700

    delete garbage

--- learning/src/main/java/rali/ShowConditions.java
+++
@@ -1,8 +0,0 @@
-package rali;
-
-public class ShowConditions {
-	public static void main(String[] args) {
-		boolean condition = true;
-		System.out.println(condition ? "This is true" : "This is false");
-	}
-}
