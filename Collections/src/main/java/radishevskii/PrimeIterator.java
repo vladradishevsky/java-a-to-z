242c8ea5844fb2a65182081c0dff4275ba5d2cfe
vladradishevsky <rvl@myttk.ru>
2017-12-24 02:02:42 +0700

    Documentation added for PrimeIterator Class.

--- Collections/src/main/java/radishevskii/PrimeIterator.java
+++ Collections/src/main/java/radishevskii/PrimeIterator.java
@@ -3,21 +3,43 @@ package radishevskii;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-
+/**
+ * Class PrimeIterator.
+ * Iterate all prime integers from input array.
+ */
 public class PrimeIterator implements Iterator {
 
+    /**
+     * Field values.
+     */
     private final int[] values;
+
+    /**
+     * Field index.
+     */
     private int index = 0;
 
+    /**
+     * Constructor of PrimeIterator.
+     * @param values array of integers.
+     */
     public PrimeIterator(final int[] values) {
         this.values = this.getPrimeArray(values);
     }
 
+    /**
+     * Override method hasNext.
+     * @return true if the iteration has more prime elements.
+     */
     @Override
     public boolean hasNext() {
         return this.index < this.values.length;
     }
 
+    /**
+     * Override method next.
+     * @return the next prime number in the iteration.
+     */
     @Override
     public Object next() {
         if (!this.hasNext()) {
@@ -26,6 +48,11 @@ public class PrimeIterator implements Iterator {
         return this.values[this.index++];
     }
 
+    /**
+     * Method to check whether the number is prime.
+     * @param number number to check.
+     * @return true if number is prime.
+     */
     private boolean isPrime(int number) {
         if (number == 1) {
             return false;
@@ -38,6 +65,11 @@ public class PrimeIterator implements Iterator {
         return true;
     }
 
+    /**
+     * Inner method to filter non-prime numbers in array.
+     * @param values array of numbers.
+     * @return new array of numbers with only prime numbers.
+     */
     private int[] getPrimeArray(int[] values) {
         int[] tempArray = new int[values.length];
         int primeCount = 0;
